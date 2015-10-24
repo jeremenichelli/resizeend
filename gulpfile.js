077e1d2378b86a186c6e70bc9fdcb6469ca8c25d
Jere Menichelli <jmenichelli@gmail.com>
2015-10-24 20:55:14 -0300

    Update Karma server start call

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,109 @@
 (function() {
     'use strict';
 
+    /*
+     * aliasses
+     * gulp: gulp streaming build system
+     * $: namespace for gulp plugins
+     */
     var gulp = require('gulp'),
-        rename = require('gulp-rename'),
-        eslint = require('gulp-eslint'),
-        uglify = require('gulp-uglify'),
-        karma = require('gulp-karma'),
-        concat = require('gulp-concat-util'),
-        project = require('./package.json');
+        $ = require('gulp-load-plugins')(),
+        // project information
+        project = require('./package.json'),
+        // karma test server
+        karmaServer = require('karma').Server;
 
-    // project paths
+    /*
+     * gulp plugins available
+     *
+     * $.rename: gulp-rename
+     * $.uglify: gulp-uglify
+     * $.karma: gulp-karma
+     * $.concatUtil: gulp-concat-util
+     * $.eslint: gulp-eslint
+     */
+
+    /*
+     * project paths
+     */
     var paths = {
         src: './src/' + project.name + '.js',
         spec: './test/' + project.name + '.spec.js',
         output: './dist'
     }
 
-    // banner with project info
+    /*
+     * project banner for dist version
+     */
     var banner = '/*' +
         '\n * ' + project.title + ' - v' + project.version +
         '\n * ' + project.url +
         '\n * ' + project.copyright + ' (c) ' + project.author + ' - ' + project.license + ' License' +
         '\n*/\n\n';
 
-    // tasks
-    gulp.task('hint:src', function() {
+    /*
+     * checks syntax in source files
+     *
+     * gulp lint:src
+     */
+    gulp.task('lint:src', function() {
         return gulp.src(paths.src)
-            .pipe(eslint())
-            // outputs the lint results to the console. 
-            .pipe(eslint.format())
-            // To have the process exit with an error code (1) on 
-            // lint error, return the stream and pipe to failOnError last. 
-            .pipe(eslint.failOnError());
+            .pipe($.eslint())
+            .pipe($.eslint.format())
+            .pipe($.eslint.failAfterError());
     });
 
-    gulp.task('hint:spec', function() {
+    /*
+     * checks syntax in test files
+     *
+     * gulp lint:src
+     */
+    gulp.task('lint:spec', function() {
         return gulp.src(paths.spec)
-            .pipe(eslint())
-            // outputs the lint results to the console. 
-            .pipe(eslint.format())
-            // To have the process exit with an error code (1) on 
-            // lint error, return the stream and pipe to failOnError last. 
-            .pipe(eslint.failOnError());
+            .pipe($.eslint())
+            .pipe($.eslint.format())
+            .pipe($.eslint.failAfterError());
     });
 
-    gulp.task('hint', [ 'hint:src', 'hint:spec' ]);
+    /*
+     * groups lint tasks
+     *
+     * gulp lint
+     */
+    gulp.task('lint', [ 'lint:src', 'lint:spec' ]);
 
-    gulp.task('test', [ 'hint' ], function() {
-        return gulp.src([ paths.spec, paths.src ])
-            .pipe(karma({configFile: 'test/karma.conf.js'}));
+    /*
+     * checks general syntax and run tests on source files
+     *
+     * gulp test
+     */
+    gulp.task('test', [ 'lint' ], function(done) {
+        var server = new karmaServer({
+            configFile: __dirname + '/test/karma.conf.js',
+            singleRun: true
+        }, done);
+
+        return server.start();
     });
 
+    /*
+     * run tests and creates distribution files
+     *
+     * gulp build
+     */
     gulp.task('build', [ 'test' ], function () {
         return gulp.src(paths.src)
-            .pipe(concat.header(banner))
+            .pipe($.concatUtil.header(banner))
             .pipe(gulp.dest(paths.output))
-            .pipe(uglify())
-            .pipe(rename({
+            .pipe($.uglify())
+            .pipe($.rename({
                 suffix: '.min'
             }))
             .pipe(gulp.dest(paths.output));
     });
 
-    // default task
+    /*
+     * default task */
     gulp.task('default', [ 'build' ]);
 
 })();
